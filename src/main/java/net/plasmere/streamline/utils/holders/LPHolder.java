d0dd057f131445924fe67172d4338c1168ec3024
Tal A. Baskin <talbaskin.business@gmail.com>
2022-05-06 17:35:53 +0300

    Update LPHolder.java
    Takes use of the new AbstractHolder which allows for way easier expandability. and saves lines :)

--- src/main/java/net/plasmere/streamline/utils/holders/LPHolder.java
+++ src/main/java/net/plasmere/streamline/utils/holders/LPHolder.java
@@ -1,30 +1,15 @@
 package net.plasmere.streamline.utils.holders;
 
-import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
-import net.md_5.bungee.api.ProxyServer;
-import net.plasmere.streamline.StreamLine;
 import net.plasmere.streamline.utils.MessagingUtils;
 
-public class LPHolder {
-    public LuckPerms api;
-    public boolean enabled;
+public class LPHolder extends AbstractHolder<LuckPermsProvider> {
 
-    public LPHolder(){
-        enabled = isPresent();
+    public LPHolder() {
+        super("LuckPerms");
+        if(super.isPresent())
+            try {super.setAPI(LuckPermsProvider.get().getClass());}
+            catch(Exception e) {MessagingUtils.logSevere("LuckPerms not loaded... Disabling LuckPerms support...");} 
     }
 
-    public boolean isPresent(){
-        if (ProxyServer.getInstance().getPluginManager().getPlugin("LuckPerms") == null) {
-            return false;
-        }
-
-        try {
-            api = LuckPermsProvider.get();
-            return true;
-        } catch (Exception e) {
-            MessagingUtils.logSevere("LuckPerms not loaded... Disabling LuckPerms support...");
-        }
-        return false;
-    }
 }
